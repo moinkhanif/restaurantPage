1e8db93ec5f5dea44aa2df2270b63533042a3201
Moin <kmoin786@gmail.com>
2020-08-06 03:04:30 +0530

    Style header and add font and color

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,18 @@ module.exports = {
           'sass-loader',
         ],
       },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader',
+        ],
+      },
     ],
   }
 };
