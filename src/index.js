d389345ad3d08b05ee1e9cda2f936ee7246d16fd
Moin <kmoin786@gmail.com>
2020-09-17 02:21:48 +0530

    add smooth scroll to page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './styles.scss';
 
+document.querySelectorAll('.menu-item').forEach((menuItem) => {
+  menuItem.addEventListener('click', () => {
+    const { menu } = menuItem.dataset;
+    if (menu) {
+      document.querySelector(menu).scrollIntoView({
+        behavior: 'smooth',
+      });
+    }
+  });
+});
 console.log("It's exciting to start a new restaurant!");
