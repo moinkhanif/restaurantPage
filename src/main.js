6a78327dde7fcd71eb00eb4b88628898e97eeafd
Moin <kmoin786@gmail.com>
2020-09-18 05:04:31 +0530

    add heading to the homepage

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-const ut = require('./unitools.js');
+const { content, createEl } = require('./unitools.js');
 
 const main = () => {
-  const main = ut.content().appendChild(document.createElement('main'));
-  const firstServe = main.appendChild(document.createElement('div'));
+  const main = content().appendChild(createEl('main'));
+  const firstServe = main.appendChild(createEl('div'));
   firstServe.className = 'first-serve';
+  const fsP = firstServe.appendChild(createEl('p'));
+  fsP.innerHTML = 'A Premium <br> And <span class="title-complement">Authentic</span> Serve <br> For the likes of You';
+  fsP.appendChild(createEl('br'));
+  const fspButton = fsP.appendChild(createEl('button'));
+  fspButton.innerHTML = 'Book A Table';
+
+  // Picture section
 };
 export default main;
